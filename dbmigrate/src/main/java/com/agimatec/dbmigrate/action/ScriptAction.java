861e1a0e0cfece6a4b5faa1feef51eaaf8d1a0be
roman.stumm <roman.stumm@67051842-bf4d-0410-baec-096b3ad6031c>
2008-12-01 10:49:05 +0000

    NC-255: invoke command or scripts via command line arguments

---
+++ dbmigrate/src/main/java/com/agimatec/dbmigrate/action/ScriptAction.java
@@ -0,0 +1,43 @@
+package com.agimatec.dbmigrate.action;
+
+import com.agimatec.dbmigrate.AutoMigrationTool;
+
+/**
+ * Description: <br/>
+ * User: roman.stumm <br/>
+ * Date: 01.12.2008 <br/>
+ * Time: 10:26:14 <br/>
+ * Copyright: Agimatec GmbH
+ */
+public abstract class ScriptAction extends MigrateAction {
+    protected String scriptName;
+
+    public static ScriptAction create(AutoMigrationTool tool, String fileName, String fileType) {
+        if ("xml".equalsIgnoreCase(fileType)) {
+            return new XmlScriptAction(tool, fileName);
+        } else if ("sql".equalsIgnoreCase(fileType)) {
+            return new SqlScriptAction(tool, fileName);
+        } else if ("groovy".equalsIgnoreCase(fileType)) {
+            return new GroovyScriptAction(tool, fileName);
+        } else {
+            tool.log("not a supported file type: " + fileName);
+            return null;
+        }
+    }
+
+    public static ScriptAction create(AutoMigrationTool tool, String fileName) {
+        int idx = fileName.lastIndexOf('.');
+        if (idx == -1) return null;
+        String fileType = fileName.substring(idx + 1);
+        return create(tool, fileName, fileType);
+    }
+
+    protected ScriptAction(AutoMigrationTool tool, String scriptName) {
+        super(tool);
+        this.scriptName = scriptName;
+    }
+
+    public String getScriptName() {
+        return scriptName;
+    }
+}
---
+++ dbmigrate/src/main/java/com/agimatec/dbmigrate/action/XmlScriptAction.java
@@ -0,0 +1,27 @@
+package com.agimatec.dbmigrate.action;
+
+import com.agimatec.dbmigrate.AutoMigrationTool;
+
+/**
+ * Description: <br/>
+ * User: roman.stumm <br/>
+ * Date: 01.12.2008 <br/>
+ * Time: 10:44:18 <br/>
+ * Copyright: Agimatec GmbH
+ */
+public class XmlScriptAction extends ScriptAction {
+    public XmlScriptAction(AutoMigrationTool tool, String scriptCmd) {
+        super(tool, scriptCmd);
+    }
+
+    @Override
+    public void doIt() throws Exception {
+        String filePath = tool.getScriptsDir() + getScriptName();
+        tool.doXmlScript(filePath);
+    }
+
+    public String getInfo() {
+        String filePath = tool.getScriptsDir() + getScriptName();
+        return "XML Config: " + filePath;
+    }
+}
