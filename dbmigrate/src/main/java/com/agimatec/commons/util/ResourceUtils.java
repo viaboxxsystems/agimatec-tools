769f6f6061cddee663c2f9363e03a923e31fe0d6
roman.stumm <roman.stumm@67051842-bf4d-0410-baec-096b3ad6031c>
2010-03-29 14:08:27 +0000

    unused imports removed

--- dbmigrate/src/main/java/com/agimatec/commons/util/ResourceUtils.java
+++ dbmigrate/src/main/java/com/agimatec/commons/util/ResourceUtils.java
@@ -1,17 +1,14 @@
 package com.agimatec.commons.util;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLDecoder;
-import java.util.*;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.regex.Pattern;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
-import java.util.zip.ZipFile;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 /**
  * Description: <br>
