aca4d40f168a919223f839f356dc4894185475ce
roman.stumm@gmail.com <roman.stumm@gmail.com@67051842-bf4d-0410-baec-096b3ad6031c>
2012-11-07 13:55:28 +0000

    postgres 9.1 is a little picky when committing a transaction that is in autoCommit-mode, so avoid this

--- dbmigrate/src/main/java/com/agimatec/jdbc/JdbcDatabaseImpl.java
+++ dbmigrate/src/main/java/com/agimatec/jdbc/JdbcDatabaseImpl.java
@@ -16,108 +16,112 @@ import java.util.Properties;
  * Copyright: Agimatec GmbH
  */
 class JdbcDatabaseImpl implements JdbcDatabase {
-  protected static final Log log = LogFactory.getLog(JdbcDatabaseImpl.class);
+    protected static final Log log = LogFactory.getLog(JdbcDatabaseImpl.class);
 
-  private Connection connection;
-  private final Properties properties;
-  private boolean transaction = false;
+    private Connection connection;
+    private final Properties properties;
+    private boolean transaction = false;
+    private boolean autoCommit = false;
 
-  public JdbcDatabaseImpl(Properties aProperties) {
-    properties = new Properties();
-    properties.putAll(aProperties);
-  }
-
-  public Connection getConnection() {
-    return connection;
-  }
-
-  public String getDriverClassName() {
-    return properties.getProperty(JdbcConfig.JDBC_DRIVER);
-  }
-
-  public void begin() {
-    if (isTransaction()) {
-      throw new JdbcException("transaction already started");
-    } else {
-      if (connection == null) connect();
-      transaction = true;
+    public JdbcDatabaseImpl(Properties aProperties) {
+        properties = new Properties();
+        properties.putAll(aProperties);
     }
-  }
 
-  public void setConnection(Connection connection) {
-    this.connection = connection;
-    transaction = connection != null;
-  }
-
-  private void connect() {
-    try {
-      if (log.isInfoEnabled()) {
-        log.info("JdbcDriver: " + getDriverClassName() + "; JdbcConnect: " +
-            getConnectionString() + "; properties: " + properties);
-      }
-      if (getDriverClassName() != null) Class.forName(getDriverClassName());
-      connection = DriverManager.getConnection(getConnectionString(), properties);
-//            connection.setAutoCommit(false);
-    } catch (Exception e) {
-      throw new JdbcException(e);
+    public Connection getConnection() {
+        return connection;
     }
-  }
 
-  public boolean isTransaction() {
-    return connection != null && transaction;
-  }
-
-  public void rollback() {
-    if (isTransaction()) {
-      try {
-        connection.rollback();
-      } catch (SQLException e) {
-        throw new JdbcException(e);
-      } finally {
-        transaction = false;
-      }
-    } else {
-      throw new JdbcException("cannot rollback - transaction not started");
+    public String getDriverClassName() {
+        return properties.getProperty(JdbcConfig.JDBC_DRIVER);
     }
-  }
 
-  public void commit() {
-    if (isTransaction()) {
-      try {
-        connection.commit();
-      } catch (SQLException e) {
-        throw new JdbcException(e);
-      } finally {
-        transaction = false;
-      }
-    } else {
-      throw new JdbcException("cannot commit - transaction not started");
+    public void begin() {
+        if (isTransaction()) {
+            throw new JdbcException("transaction already started");
+        } else {
+            if (connection == null) connect();
+        }
     }
-  }
 
-  public Properties getProperties() {
-    return properties;
-  }
-
-  public String getConnectionString() {
-    return properties.getProperty(JdbcConfig.JDBC_URL);
-  }
-
-  public void close() {
-    if (connection != null) {
-      Connection tc = connection;
-      connection = null;
-      try {
-        tc.close();
-      } catch (SQLException e) {
-        throw new JdbcException(e);
-      }
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+        try {
+            autoCommit = connection != null && connection.getAutoCommit();
+        } catch (SQLException e) {
+        }
+        transaction = connection != null;
     }
-  }
 
-  public void init(String driver, String url, Properties props) {
-    if (driver != null) properties.setProperty(JdbcConfig.JDBC_DRIVER, driver);
-    if (url != null) properties.setProperty(JdbcConfig.JDBC_URL, url);
-    properties.putAll(props);
-  }
+    private void connect() {
+        try {
+            if (log.isInfoEnabled()) {
+                log.info("JdbcDriver: " + getDriverClassName() + "; JdbcConnect: " +
+                        getConnectionString() + "; properties: " + properties);
+            }
+            if (getDriverClassName() != null) Class.forName(getDriverClassName());
+            setConnection(DriverManager.getConnection(getConnectionString(), properties));
+//            connection.setAutoCommit(false);  // some database complains when using DDL statements in transacted mode
+        } catch (Exception e) {
+            throw new JdbcException(e);
+        }
+    }
+
+    public boolean isTransaction() {
+        return connection != null && transaction && !autoCommit;
+    }
+
+    public void rollback() {
+        if (isTransaction()) {
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                throw new JdbcException(e);
+            } finally {
+                transaction = false;
+            }
+        } else {
+            throw new JdbcException("cannot rollback - transaction not started");
+        }
+    }
+
+    public void commit() {
+        if (isTransaction()) {
+            try {
+                connection.commit();
+            } catch (SQLException e) {
+                throw new JdbcException(e);
+            } finally {
+                transaction = false;
+            }
+        } else {
+            throw new JdbcException("cannot commit - transaction not started");
+        }
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public String getConnectionString() {
+        return properties.getProperty(JdbcConfig.JDBC_URL);
+    }
+
+    public void close() {
+        if (connection != null) {
+            Connection tc = connection;
+            connection = null;
+            try {
+                tc.close();
+            } catch (SQLException e) {
+                throw new JdbcException(e);
+            }
+        }
+    }
+
+    public void init(String driver, String url, Properties props) {
+        if (driver != null) properties.setProperty(JdbcConfig.JDBC_DRIVER, driver);
+        if (url != null) properties.setProperty(JdbcConfig.JDBC_URL, url);
+        properties.putAll(props);
+    }
 }
