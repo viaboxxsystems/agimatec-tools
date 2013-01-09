2c0911e7972444c9c6ae11d95c548ea8ff2f0132
roman.stumm@gmail.com <roman.stumm@gmail.com@67051842-bf4d-0410-baec-096b3ad6031c>
2013-01-09 12:08:20 +0000

    added code to remove a column from a excel sheet

--- trunk/dbimport/src/main/java/com/agimatec/utility/fileimport/spreadsheet/ExcelRowReader.java
+++ trunk/dbimport/src/main/java/com/agimatec/utility/fileimport/spreadsheet/ExcelRowReader.java
@@ -4,6 +4,7 @@ import com.agimatec.utility.fileimport.LineReader;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
 import java.io.IOException;
@@ -35,8 +36,8 @@ public class ExcelRowReader implements LineReader<ExcelRow> {
     }
 
     /**
-     * @throws UnsupportedOperationException - no ReaderInputStream support implemented yet!
      * @param aReader
+     * @throws UnsupportedOperationException - no ReaderInputStream support implemented yet!
      * @throws IOException
      */
     public void setReader(Reader aReader) throws IOException {
@@ -57,7 +58,7 @@ public class ExcelRowReader implements LineReader<ExcelRow> {
     }
 
     protected void init() throws IOException {
-        if(rowIterator != null) return; // short-circuit
+        if (rowIterator != null) return; // short-circuit
 
         if (fileSystem == null) {
             fileSystem = new POIFSFileSystem(stream);
@@ -105,5 +106,78 @@ public class ExcelRowReader implements LineReader<ExcelRow> {
 
     public void setWorkbook(HSSFWorkbook workbook) {
         this.workbook = workbook;
+    }
+
+    // source from http://pastebin.com/ff806298
+    // unsolved issues:
+    // + What to do with the formula in the moved columns?
+    // + Column breaks
+    // + Merged regions
+    public void removeColumn(int columnToDelete) {
+        int maxColumn = 0;
+        for (int r = 0; r < sheet.getLastRowNum() + 1; r++) {
+            Row row = sheet.getRow(r);
+
+            // if no row exists here; then nothing to do; next!
+            if (row == null)
+                continue;
+
+            // if the row doesn't have this many columns then we are good; next!
+            int lastColumn = row.getLastCellNum();
+            if (lastColumn > maxColumn)
+                maxColumn = lastColumn;
+
+            if (lastColumn < columnToDelete)
+                continue;
+
+            for (int x = columnToDelete + 1; x < lastColumn + 1; x++) {
+                Cell oldCell = row.getCell(x - 1);
+                if (oldCell != null)
+                    row.removeCell(oldCell);
+
+                Cell nextCell = row.getCell(x);
+                if (nextCell != null) {
+                    Cell newCell = row.createCell(x - 1, nextCell.getCellType());
+                    cloneCell(newCell, nextCell);
+                }
+            }
+        }
+
+        // Adjust the column widths
+        for (int c = 0; c < maxColumn; c++) {
+            sheet.setColumnWidth(c, sheet.getColumnWidth(c + 1));
+        }
+    }
+
+    /*
+     * Takes an existing Cell and merges all the styles and forumla
+     * into the new one
+     */
+    private static void cloneCell(Cell cNew, Cell cOld) {
+        cNew.setCellComment(cOld.getCellComment());
+        cNew.setCellStyle(cOld.getCellStyle());
+
+        switch (cNew.getCellType()) {
+            case Cell.CELL_TYPE_BOOLEAN:
+                cNew.setCellValue(cOld.getBooleanCellValue());
+                break;
+
+            case Cell.CELL_TYPE_NUMERIC:
+                cNew.setCellValue(cOld.getNumericCellValue());
+                break;
+
+            case Cell.CELL_TYPE_STRING:
+                cNew.setCellValue(cOld.getStringCellValue());
+                break;
+
+            case Cell.CELL_TYPE_ERROR:
+                cNew.setCellValue(cOld.getErrorCellValue());
+                break;
+
+            case Cell.CELL_TYPE_FORMULA:
+                cNew.setCellFormula(cOld.getCellFormula());
+                break;
+        }
+
     }
 }
