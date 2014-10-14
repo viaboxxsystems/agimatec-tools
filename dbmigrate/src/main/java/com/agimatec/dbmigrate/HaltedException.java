1df84713f3dbbacc0b3a3b83262c9b6ef42fea7a
roman.stumm@googlemail.com <roman.stumm@googlemail.com@67051842-bf4d-0410-baec-096b3ad6031c>
2014-10-14 15:41:19 +0000

    added busy-lock feature to prevent parallel execution of dbmigrate

--- dbmigrate/src/main/java/com/agimatec/dbmigrate/HaltedException.java
+++ dbmigrate/src/main/java/com/agimatec/dbmigrate/HaltedException.java
@@ -11,4 +11,13 @@ public class HaltedException extends RuntimeException
     public HaltedException(String message) {
         super(message);
     }
+
+    /**
+     * @since 2.5.19
+     * @param s
+     * @param throwable
+     */
+    public HaltedException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
 }
--- dbmigrate/src/main/java/com/agimatec/dbmigrate/util/DBVersionMeta.java
+++ dbmigrate/src/main/java/com/agimatec/dbmigrate/util/DBVersionMeta.java
@@ -12,147 +12,197 @@ import java.io.Serializable;
  * Time: 10:31:21 <br/>
  */
 public class DBVersionMeta implements Serializable {
-  private String tableName = "DB_VERSION";   // mandatory
-  private String column_version = "version"; // mandatory
-  private String column_since = "since";   // optional
-  // optional: when true, version will always be inserted (journalling)
+    private String tableName = "DB_VERSION";   // mandatory
+    private String column_version = "version"; // mandatory
+    private String column_since = "since";   // optional
+    // optional: when true, version will always be inserted (journalling)
 
-  private boolean insertOnly = false;
-  /**
-   * true: create db_version table if absent
-   * false: do not create, requires manual setup
-   */
-  private boolean autoCreate = true;
-  /**
-   * true: set version automatically after a script finishes
-   * false: requires call to UpdateVersionScriptVisitor to persist a new db_version
-   */
-  private boolean autoVersion = false;
+    private boolean insertOnly = false;
+    /**
+     * true: create db_version table if absent
+     * false: do not create, requires manual setup
+     */
+    private boolean autoCreate = true;
+    /**
+     * true: set version automatically after a script finishes
+     * false: requires call to UpdateVersionScriptVisitor to persist a new db_version
+     */
+    private boolean autoVersion = false;
 
-  private String sqlInsert, sqlSelect, sqlUpdate; // cache
-
-  public boolean isAutoCreate() {
-    return autoCreate;
-  }
-
-  public void setAutoCreate(boolean autoCreate) {
-    this.autoCreate = autoCreate;
-  }
-
-  public boolean isAutoVersion() {
-    return autoVersion;
-  }
-
-  public void setAutoVersion(boolean autoVersion) {
-    this.autoVersion = autoVersion;
-  }
-
-  public String getTableName() {
-    return tableName;
-  }
-
-  public void setTableName(String tableName) {
-    this.tableName = tableName;
-    resetCache();
-  }
-
-  public String getColumn_version() {
-    return column_version;
-  }
-
-  public void setColumn_version(String column_version) {
-    this.column_version = column_version;
-    resetCache();
-  }
-
-  protected void resetCache() {
-    sqlInsert = null;
-    sqlSelect = null;
-    sqlUpdate = null;
-  }
-
-  /**
-   * @return column name or null
-   */
-  public String getColumn_since() {
-    return column_since;
-  }
-
-  public void setColumn_since(String column_since) {
-    this.column_since = column_since;
-    resetCache();
-  }
-
-  public String getQualifiedVersionColumn() {
-    return tableName + "." + column_version;
-  }
-
-  public String toSQLInsert() {
-    if (sqlInsert == null) {
-      StringBuilder sql = new StringBuilder();
-      sql.append("INSERT INTO ");
-      sql.append(getTableName());
-      sql.append("(").append(getColumn_version());
-      if (StringUtils.isNotEmpty(getColumn_since())) {
-        sql.append(", ");
-        sql.append(getColumn_since());
-      }
-      sql.append(") VALUES(?");
-      if (StringUtils.isNotEmpty(getColumn_since())) {
-        sql.append(",?");
-      }
-      sql.append(")");
-      sqlInsert = sql.toString();
+    /**
+     * Wait/Fail: write a busy-version into the 'table' while migration is running. This is to prevent
+     * the migration tool to run multiple times in parallel.
+     * Fail: fail when busy
+     * Wait: wait until not busy anymore, then start
+     * <p/>
+     * No: do not care about parallel runs. The user must himself avoid to run multiple dbmigrate processes at the same
+     * database at the same time.
+     *
+     * @since 2.5.19
+     */
+    public enum LockBusy {
+        No, Wait, Fail
     }
-    return sqlInsert;
-  }
 
-  public String toSQLUpdate() {
-    if (sqlUpdate == null) {
-      StringBuilder sql = new StringBuilder();
-      sql.append("UPDATE ");
-      sql.append(getTableName());
-      sql.append(" SET ");
-      sql.append(getColumn_version());
-      sql.append("=?");
-      if (StringUtils.isNotEmpty(getColumn_since())) {
-        sql.append(", ");
-        sql.append(getColumn_since());
-        sql.append("=?");
-      }
-      sqlUpdate = sql.toString();
+    private LockBusy lockBusy = LockBusy.No;
+
+    private String sqlInsert, sqlSelect, sqlUpdate; // cache
+
+    public boolean isAutoCreate() {
+        return autoCreate;
     }
-    return sqlUpdate;
-  }
 
-  public String toSQLCreateTable() {
-    StringBuilder buf = new StringBuilder();
-    buf.append("CREATE TABLE ").append(getTableName()).append(" (");
-    if (StringUtils.isNotEmpty(getColumn_since())) {
-      buf.append(getColumn_since()).append(" TIMESTAMP, ");
+    public void setAutoCreate(boolean autoCreate) {
+        this.autoCreate = autoCreate;
     }
-    buf.append(getColumn_version()).append(" VARCHAR(100) NOT NULL)");
-    return buf.toString();
-  }
 
-  /**
-   * SQL-Statement zum Holen der neusten (=zuletzt gespeicherten) Version
-   */
-  public String toSQLSelectVersion() {
-    if (sqlSelect == null) {
-      sqlSelect = "SELECT " + getColumn_version() + " FROM " + getTableName();
-      if (StringUtils.isNotEmpty(getColumn_since())) {
-        sqlSelect = sqlSelect + " ORDER BY " + getColumn_since() + " DESC";
-      }
+    public boolean isAutoVersion() {
+        return autoVersion;
     }
-    return sqlSelect;
-  }
 
-  public void setInsertOnly(boolean aBoolean) {
-    insertOnly = aBoolean;
-  }
+    public void setAutoVersion(boolean autoVersion) {
+        this.autoVersion = autoVersion;
+    }
 
-  public boolean isInsertOnly() {
-    return insertOnly;
-  }
+    /**
+     * @return
+     * @since 2.5.19
+     */
+    public LockBusy getLockBusy() {
+        return lockBusy;
+    }
+
+    /**
+     * @param lockBusy
+     * @since 2.5.19
+     */
+    public void setLockBusy(LockBusy lockBusy) {
+        this.lockBusy = lockBusy;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+        resetCache();
+    }
+
+    public String getColumn_version() {
+        return column_version;
+    }
+
+    public void setColumn_version(String column_version) {
+        this.column_version = column_version;
+        resetCache();
+    }
+
+    protected void resetCache() {
+        sqlInsert = null;
+        sqlSelect = null;
+        sqlUpdate = null;
+    }
+
+    /**
+     * @return column name or null
+     */
+    public String getColumn_since() {
+        return column_since;
+    }
+
+    public void setColumn_since(String column_since) {
+        this.column_since = column_since;
+        resetCache();
+    }
+
+    public String getQualifiedVersionColumn() {
+        return tableName + "." + column_version;
+    }
+
+    public String toSQLInsert() {
+        if (sqlInsert == null) {
+            StringBuilder sql = new StringBuilder();
+            sql.append("INSERT INTO ");
+            sql.append(getTableName());
+            sql.append("(").append(getColumn_version());
+            if (StringUtils.isNotEmpty(getColumn_since())) {
+                sql.append(", ");
+                sql.append(getColumn_since());
+            }
+            sql.append(") VALUES(?");
+            if (StringUtils.isNotEmpty(getColumn_since())) {
+                sql.append(",?");
+            }
+            sql.append(")");
+            sqlInsert = sql.toString();
+        }
+        return sqlInsert;
+    }
+
+    public String toSQLUpdate() {
+        if (sqlUpdate == null) {
+            StringBuilder sql = new StringBuilder();
+            sql.append("UPDATE ");
+            sql.append(getTableName());
+            sql.append(" SET ");
+            sql.append(getColumn_version());
+            sql.append("=?");
+            if (StringUtils.isNotEmpty(getColumn_since())) {
+                sql.append(", ");
+                sql.append(getColumn_since());
+                sql.append("=?");
+            }
+            sqlUpdate = sql.toString();
+        }
+        return sqlUpdate;
+    }
+
+    /**
+     * @return
+     * @since 2.5.19
+     */
+    public String toSQLDelete() {
+        if (sqlUpdate == null) {
+            StringBuilder sql = new StringBuilder();
+            sql.append("DELETE FROM ");
+            sql.append(getTableName());
+            sql.append(" WHERE ");
+            sql.append(getColumn_version());
+            sql.append("=?");
+            sqlUpdate = sql.toString();
+        }
+        return sqlUpdate;
+    }
+
+    public String toSQLCreateTable() {
+        StringBuilder buf = new StringBuilder();
+        buf.append("CREATE TABLE ").append(getTableName()).append(" (");
+        if (StringUtils.isNotEmpty(getColumn_since())) {
+            buf.append(getColumn_since()).append(" TIMESTAMP, ");
+        }
+        buf.append(getColumn_version()).append(" VARCHAR(100) NOT NULL)");
+        return buf.toString();
+    }
+
+    /**
+     * SQL-Statement zum Holen der neusten (=zuletzt gespeicherten) Version
+     */
+    public String toSQLSelectVersion() {
+        if (sqlSelect == null) {
+            sqlSelect = "SELECT " + getColumn_version() + " FROM " + getTableName();
+            if (StringUtils.isNotEmpty(getColumn_since())) {
+                sqlSelect = sqlSelect + " ORDER BY " + getColumn_since() + " DESC";
+            }
+        }
+        return sqlSelect;
+    }
+
+    public void setInsertOnly(boolean aBoolean) {
+        insertOnly = aBoolean;
+    }
+
+    public boolean isInsertOnly() {
+        return insertOnly;
+    }
 }
